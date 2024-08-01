d4d5f5d56c7f451d8d48fb4a5b87c95efa0bb674
Elfansyah <elfansyah91@gmail.com>
2024-08-01 12:03:30 +0700

    Update Beranda

--- src/app/beranda/components/mid.tsx
+++ src/app/beranda/components/mid.tsx
@@ -1,33 +1,51 @@
+'use client'
+import Image from "next/image"
 import Link from "next/link"
-
-Link
+import img1 from "@/assets/Foto 1.jpg"
+import { FaRegImage } from "react-icons/fa6";
+import { MdGifBox } from "react-icons/md";
+import { BiPoll } from "react-icons/bi";
+import { MdOutlineEmojiEmotions } from "react-icons/md";
+import { RiCalendarScheduleFill } from "react-icons/ri";
+import { FaLocationDot } from "react-icons/fa6";
+import { AiFillLike } from "react-icons/ai";
+import { AiFillDislike } from "react-icons/ai";
+import { useState } from "react";
 
 export default function Mid() {
+    const [like,setLike] = useState(0)
     return (
-        <div className=" pl-10 pr-10 bg-white">
+        <div className="bg-white w-full">
             {/* Judul */}
-            <div className="flex gap-5 justify-center ">
-                <Link href="/" className="text-primary font-semibold border border-solid border-primary py-1 px-48 hover:bg-primary hover:text-white">For You</Link>
-                <Link href="/" className="text-primary font-semibold border border-solid border-primary py-1 px-48 hover:bg-primary hover:text-white">Following</Link>
+            <div className="flex justify-center ">
+                <Link href="/" className="text-primary font-semibold border border-solid border-primary py-1 px-[188px] hover:bg-primary hover:text-white">For You</Link>
+                <Link href="/" className="text-primary font-semibold border border-solid border-primary py-1 px-[188px] hover:bg-primary hover:text-white">Following</Link>
             </div>
 
             {/* What Is Happening?! */}
             <div className="border border-solid border-gray-300 p-5">
                 <div className="flex gap-3">
-                    <div className="avatar pt-5 pl-2">
-                        <div className="w-14 rounded-full">
-                            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    <div>
+                        <div className="avatar pt-5">
+                            <Image src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" alt="profile" width={50} height={50} className="rounded-full" />
                         </div>
                     </div>
-                    <div className="pt-8">
-                        <input type="text" className="text-xl bg-white" placeholder="What Is Happening?!" />
+                    <div className="pt-8 w-full">
+                        <textarea className="bg-white text-black text-xl w-full outline-none" placeholder="What Is Happening?!" />
                     </div>
                 </div>
-                <input type="file" className="pt-5" />
-                <div className="pt-10 flex justify-end">
-                    <Link href="/" className="py-3 px-6 bg-primary text-white rounded-full text-base font-semibold hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out">
+                <div className="pt-10 flex justify-between">
+                    <div className="flex gap-3 pt-3 pl-5">
+                        <FaRegImage size={20} color="gray" />
+                        <MdGifBox size={20} color="gray" />
+                        <BiPoll size={20} color="gray" />
+                        <MdOutlineEmojiEmotions size={20} color="gray" />
+                        <RiCalendarScheduleFill size={20} color="gray" />
+                        <FaLocationDot size={20} color="gray" />
+                    </div>
+                    <button className="py-3 px-6 bg-primary text-white rounded-full text-base font-semibold hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out">
                         Post
-                    </Link>
+                    </button>
                 </div>
             </div>
 
@@ -46,6 +64,16 @@ export default function Mid() {
                 </div>
                 <div className="pl-16">
                     <p className="text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores nostrum delectus id obcaecati consequuntur repellendus ex, debitis dignissimos nemo consectetur minima aliquid at repudiandae iste quis enim assumenda odio veniam corrupti? Dolorem, labore! Sed natus inventore eum, unde quae nobis excepturi doloremque nesciunt eligendi perferendis, non aspernatur soluta. Odio, amet dolor voluptate, aut eveniet veniam quo cupiditate accusantium laborum quidem nam laboriosam consequuntur vero! Odio optio laboriosam nobis, dolore, minima soluta placeat facilis, quibusdam sed atque esse dolorum quidem assumenda corrupti accusamus ea nemo architecto temporibus alias quia hic. Nisi sunt minus praesentium iste quos odit ea quas doloremque quo!</p>
+                </div>
+                <div className="pl-14 pt-3 flex gap-5">
+                    <div className="flex">
+                        <button onClick={()=>setLike(like + 1)}><AiFillLike size={20} className="text-primary" /></button>
+                        <p>{like}</p>
+                    </div>
+                    <div className="flex">
+                        <button onClick={()=>setLike(like + 1)}><AiFillDislike size={20} className="text-red-500" /></button>
+                        <p>{like}</p>
+                    </div>
                 </div>
             </div>
             {/* Posting 2 */}
@@ -82,9 +110,6 @@ export default function Mid() {
                     <p className="text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores nostrum delectus id obcaecati consequuntur repellendus ex, debitis dignissimos nemo consectetur minima aliquid at repudiandae iste quis enim assumenda odio veniam corrupti? Dolorem, labore! Sed natus inventore eum, unde quae nobis excepturi doloremque nesciunt eligendi perferendis, non aspernatur soluta. Odio, amet dolor voluptate, aut eveniet veniam quo cupiditate accusantium laborum quidem nam laboriosam consequuntur vero! Odio optio laboriosam nobis, dolore, minima soluta placeat facilis, quibusdam sed atque esse dolorum quidem assumenda corrupti accusamus ea nemo architecto temporibus alias quia hic. Nisi sunt minus praesentium iste quos odit ea quas doloremque quo!</p>
                 </div>
             </div>
-
-
-
         </div>
     )
 }
--- src/app/beranda/components/sidecontent.tsx
+++ src/app/beranda/components/sidecontent.tsx
@@ -1,6 +1,6 @@
 export default function SideContent () {
     return (
-        <div className="pt-10 pr-5 bg-white">
+        <div className="pt-5 pl-5 pr-5 bg-white">
             <div>
             <input type="text" className="py-3 p-1 px-10 rounded-full border-solid border-2 border-sky-200" placeholder="Search" />
             </div>
